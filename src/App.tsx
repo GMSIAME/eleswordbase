d7f4bb7195426b26436df527d2a53d5885333c69
SIAME GM <bhs2874@naver.com>
2022-03-13 08:22:59 +0900

    20220313-Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path="/" element={(<>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <Link to="/about">About</Link>
+          </>)} />
+          <Route path="/about" element={(<>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <Link to="/">Home</Link>
+          </>)} />
+        </Routes>
+        
       </header>
     </div>
   );
